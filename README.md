03060381ba65ceb107ca51a4d444585558d13231
everypreciousday <45137802+everypreciousday@users.noreply.github.com>
2023-02-24 23:25:31 +0900

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # github-contents
+
+## alice
+
+https://www.gutenberg.org/cache/epub/11/pg11-images.html
+
+## Metamorphosis
+
+https://www.gutenberg.org/cache/epub/5200/pg5200-images.html
--- check.py
+++ check.py
@@ -1,30 +1,7 @@
 # -*- conding: utf-8 -*-
-import time
-import pprint
-import re
-import os.path
-from pathlib import Path
-import subprocess
 import traceback
-import os
-import sys
-import logging
 import yaml
-pp = pprint.PrettyPrinter(indent=4)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-stdout_handler = logging.StreamHandler(sys.stdout)
-stdout_handler.setLevel(logging.DEBUG)
-stdout_handler.setFormatter(formatter)
-
-file_handler = logging.FileHandler(f"log.txt", encoding="UTF-8")
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stdout_handler)
 
 def main():
     with open('versions.yml', encoding='UTF8') as file:
@@ -38,11 +15,11 @@ def main():
             book_language = book_obj['language']
             book_chapters = book_obj['chapters']
 
-            print(book_version)
-            print(book_language)
+            print(f"book_version:{book_version}")
+            print(f"book_language:{book_language}")
             for book_chapter in book_chapters:
                 filename = book_chapter['filename']
-                print(filename)
+                print(f"filename:{filename}")
                 if ".yml" not in filename:
                     continue
                 with open(book + "/" + filename, encoding='utf-8') as file:
@@ -57,14 +34,15 @@ def main():
                         lines = para['lines']
                         for line in lines:
                             print(line)
-                        vocas = para['vocas']
-                        for voca in vocas:
-                            print(voca)
+                        if 'vocas' in para:
+                            vocas = para['vocas']
+                            for voca in vocas:
+                                print(voca)
 
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        logger.error(e)
+        print(e)
         print(traceback.format_exc())
